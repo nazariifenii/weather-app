07dd16180c153314f6e273959d19e9810a8d874f
Nazarii Fenii <nazar.fenii@gmail.com>
2023-03-28 18:07:01 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { OPEN_WEATHER_MAP_API_URL } from "@env";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{OPEN_WEATHER_MAP_API_URL}</Text>
     </View>
   );
 }
@@ -11,8 +12,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
