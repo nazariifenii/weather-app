5ee453e83b2ed4cb9337a9ee2e726332ab696974
Nazarii Fenii <nazar.fenii@gmail.com>
2023-03-29 18:32:11 +0200

    Removed unused data in init state

--- src/store/initialState.ts
+++ src/store/initialState.ts
@@ -1,11 +1,9 @@
 export type RootState = {
-  isFetching: boolean;
   weather: ParseWeatherForecast;
   errors: { message: string };
 };
 
 const INITIAL_STATE: RootState = {
-  isFetching: true,
   weather: { weatherByDate: {}, weatherDatesByWeek: [] },
   errors: { message: "" },
 };
